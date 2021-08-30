55a1bd6d2ce79a842de6182d1194fd2525aa8ec1
Renan <dohko.renan@gmail.com>
2021-08-30 14:30:41 -0300

    Exibindo contatos

--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contato } from 'src/app/models/Contato';
 
 @Component({
   selector: 'app-lista-de-contatos',
@@ -6,6 +7,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-de-contatos.component.css']
 })
 export class ListaDeContatosComponent implements OnInit {
+
+  contatos: Contato[] = [
+    {
+      nome: "Kael'thas Sunstrider",
+      email: "kaelthas@wow.com",
+      tels: ["6666-6666", "6666-5555"],
+    },
+    {
+      nome: "Thrall",
+      email: "thrall@wow.com",
+      tels: ["0800-3666", "0842-6637"],
+    },
+    {
+      nome: "Baine Cloudhoof",
+      email: "baine@wow.com",
+      tels: ["0800-7777"],
+    },
+    {
+      nome: "Sylvanas Windrunner",
+      email: "sylvanas@wow.com",
+      tels: ["6969-6969"],
+    },
+    {
+      nome: "Varian Wrynn",
+      email: "varian@wow.com",
+      tels: ["9999-9999"],
+    },
+    {
+      nome: "Illidan Stormrage",
+      email: "illidan@wow.com",
+      tels: ["9999-9999"],
+    },
+    {
+      nome: "Malfurion Stormrage",
+      email: "malfurion@wow.com",
+      tels: ["9999-9999"],
+    },
+    {
+      nome: "Tyrande Whisperwind",
+      email: "tyrande@wow.com",
+      tels: ["9999-9999"],
+    },
+    {
+      nome: "Arthas Menethil",
+      email: "arthas@wow.com",
+      tels: ["9999-9999"],
+    },
+    {
+      nome: "Vol'Jin Darkspear",
+      email: "voljin@wow.com",
+      tels: ["9999-9999"],
+    },
+    {
+      nome: "Gazlowe, the Boss of Ratchet",
+      email: "Gazlowe@wow.com",
+      tels: ["9999-9999"],
+    },
+   ]
 
   constructor() { }
 
